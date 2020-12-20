de75c3509a30d8365895ad5dadeda68e69108569
ashen-develops <ashenp.designs@gmail.com>
2020-12-20 14:16:44 -0600

    updated pets router and service

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -9,10 +9,15 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // Return all pets currently up for adoption.
+  res.json(Pets.get());
 });
 
 router.delete('/', json, (req, res) => {
   // Remove a pet from adoption.
+  const { type } = req.params;
+  if (type !== 'cat' && type !== 'dog') return res.status(400).json({ message: 'type must be cat or dog'});
+  Pets.dequeue(req.params.type);
+  res.status(204).end();
 });
 
 module.exports = router;
--- modules/pets/pets.service.js
+++ modules/pets/pets.service.js
@@ -18,9 +18,11 @@ store.dogs.forEach(dog => pets.dogs.enqueue(dog));
 module.exports = {
   get() {
     // Return the pets next in line to be adopted.
+    return { cat: pets.cats.show(), dog: pets.dogs.show() };
   },
 
   dequeue(type) {
     // Remove a pet from the queue.
+    return type === 'cat' ? pets.cats.dequeue() : pets.dogs.dequeue();
   }
 };
