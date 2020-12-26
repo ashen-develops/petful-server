77dbb7dcc6b2308eb7b525973e137d4941a3a904
ashen-develops <ashenp.designs@gmail.com>
2020-12-26 09:44:02 -0600

    updated port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'production',
   API_PATH: process.env.API_PATH,
 };
