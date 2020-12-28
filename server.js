bb6cf57d42a75ea94bf26b8f850947102b2a284e
ashen-develops <ashenp.designs@gmail.com>
2020-12-28 17:10:27 -0600

    heroku ready

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const app = require('./modules/app/app');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`[petful-server] Listening on ${ port }.`);
